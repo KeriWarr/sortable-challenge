906e812d279a3d580c9b733cdbb1794a091b4b08
Keri Warr <keri@warr.ca>
2016-12-09 19:15:54 -0800

    Fix dev run command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { createReadStream, createWriteStream } from 'fs';
 import _ from 'lodash';
 import { createInterface } from 'readline';
 
+
 const PRODUCTS_FILE = 'products.txt';
 const LISTINGS_FILE = 'listings.txt';
 const RESULTS_FILE = 'results.txt';
@@ -20,39 +21,46 @@ let listings = [];
 let productsLineReaderIsClosed = false;
 let listingsLineReaderIsClosed = false;
 
+
 productsLineReader.on('line', line => products.push(JSON.parse(line)));
 listingsLineReader.on('line', line => listings.push(JSON.parse(line)));
 
 productsLineReader.on('close', () => {
   productsLineReaderIsClosed = true;
-  if (listingsLineReaderIsClosed) dataIsLoaded();
+  if (listingsLineReaderIsClosed) onDataLoaded();
 });
 
 listingsLineReader.on('close', () => {
   listingsLineReaderIsClosed = true;
-  if (productsLineReaderIsClosed) dataIsLoaded();
+  if (productsLineReaderIsClosed) onDataLoaded();
 });
 
-function dataIsLoaded() {
+
+function onDataLoaded() {
   products.forEach(product => product.listings = []);
 
+  firstPass();
+
+  products.forEach(product => {
+    resultsWriteStream.write(`${JSON.stringify(
+      _.pick(product, 'product_name', 'listings')
+    )}\n`);
+  });
+}
+
+function firstPass() {
   listings.forEach(listing => {
     products.forEach(product => {
       const productManufacturerRegex =
         new RegExp(`^${product.manufacturer}$`, 'i');
       const productModelRegex = new RegExp(product.model, 'i');
       const productFamilyRegex = new RegExp(product.family || '', 'i');
+
       if (productManufacturerRegex.test(listing.manufacturer)
         && productModelRegex.test(listing.title)
         && productFamilyRegex.test(listing.title)) {
         product.listings.push(listing);
       }
     });
-  });
-
-  products.forEach(product => {
-    resultsWriteStream.write(`${JSON.stringify(
-      _.pick(product, 'product_name', 'listings')
-    )}\n`);
   });
 }
