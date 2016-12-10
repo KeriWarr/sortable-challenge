aa78bc407078d0f02673e85ed93d4265aa53d6ab
Keri Warr <keri@warr.ca>
2016-12-09 21:39:07 -0800

    Functionalify

--- index.js
+++ index.js
@@ -1,56 +1,17 @@
+import R from 'ramda';
 import { createReadStream, createWriteStream } from 'fs';
-import _ from 'lodash';
 import { createInterface } from 'readline';
 
 
-const PRODUCTS_FILE = 'products.txt';
-const LISTINGS_FILE = 'listings.txt';
-const RESULTS_FILE = 'results.txt';
-
-const productsLineReader = createInterface({
-  input: createReadStream(PRODUCTS_FILE),
-});
-const listingsLineReader = createInterface({
-  input: createReadStream(LISTINGS_FILE),
-});
-const resultsWriteStream = createWriteStream(RESULTS_FILE);
-
-let products = [];
-let listings = [];
-
-let productsLineReaderIsClosed = false;
-let listingsLineReaderIsClosed = false;
+/* ----------- DATA PROCESSING ----------- */
 
 
-productsLineReader.on('line', line => products.push(JSON.parse(line)));
-listingsLineReader.on('line', line => listings.push(JSON.parse(line)));
-
-productsLineReader.on('close', () => {
-  productsLineReaderIsClosed = true;
-  if (listingsLineReaderIsClosed) onDataLoaded();
-});
-
-listingsLineReader.on('close', () => {
-  listingsLineReaderIsClosed = true;
-  if (productsLineReaderIsClosed) onDataLoaded();
-});
-
-
-function onDataLoaded() {
-  products.forEach(product => product.listings = []);
-
-  firstPass();
-
-  products.forEach(product => {
-    resultsWriteStream.write(`${JSON.stringify(
-      _.pick(product, 'product_name', 'listings')
-    )}\n`);
-  });
-}
-
-function firstPass() {
-  listings.forEach(listing => {
-    products.forEach(product => {
+/**
+ *
+ */
+const firstPass = (products, listings) =>
+  products.map(product => Object.assign({}, product, {
+    listings: listings.reduce((productListings, listing) => {
       const productManufacturerRegex =
         new RegExp(`^${product.manufacturer}$`, 'i');
       const productModelRegex = new RegExp(product.model, 'i');
@@ -59,8 +20,66 @@ function firstPass() {
       if (productManufacturerRegex.test(listing.manufacturer)
         && productModelRegex.test(listing.title)
         && productFamilyRegex.test(listing.title)) {
-        product.listings.push(listing);
+        return productListings.concat([listing]);
       }
-    });
-  });
-}
+      return productListings;
+    }, product.listings),
+  }));
+
+
+/**
+ *
+ */
+const generateResults = (products, listings) => {
+  const resultsProperties = ['product_name', 'listings'];
+  const productsWithListings = products.map(
+    product => Object.assign({},
+      product,
+      { listings: [] },
+    ),
+  );
+
+  const firstPassProducts = firstPass(productsWithListings, listings);
+
+  return firstPassProducts.map(R.pick(resultsProperties));
+};
+
+
+/* ----------- FILE PROCESSING ----------- */
+
+
+(() => {
+  const inputFiles = {
+    products: {
+      fileName: 'products.txt',
+      data: [],
+    },
+    listings: {
+      fileName: 'listings.txt',
+      data: [],
+    },
+  };
+  const resultsFile = 'results.txt';
+  const resultsWriteStream = createWriteStream(resultsFile);
+
+  const totalReaderCount = R.keys(inputFiles).length;
+  let closedReaderCount = 0;
+
+  const handleReaderClosed = () => {
+    closedReaderCount += 1;
+
+    if (closedReaderCount === totalReaderCount) {
+      const { products, listings } = inputFiles;
+
+      generateResults(products.data, listings.data).forEach(result =>
+        resultsWriteStream.write(`${JSON.stringify(result)}\n`),
+      );
+    }
+  };
+
+  R.values(inputFiles).map(({ fileName, data }) =>
+    createInterface({ input: createReadStream(fileName) })
+      .on('line', line => data.push(JSON.parse(line)))
+      .on('close', handleReaderClosed),
+  );
+})();
