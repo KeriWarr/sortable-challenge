43eafdfe2fa62a6030862d5752694092684208bb
Keri Warr <keri@warr.ca>
2017-02-28 02:50:19 -0500

    Use indexed listings

--- index.js
+++ index.js
@@ -2,99 +2,125 @@ import R from 'ramda';
 import { createReadStream, createWriteStream } from 'fs';
 import { createInterface } from 'readline';
 
-
 /* ----------- DATA PROCESSING ----------- */
-
-
-/**
- * A regex fragment that matches any character which does not contirbute
- * to the meaning of a product label (model/family).
- */
-const nonLabelChar = '[^a-z0-9]';
-
-const removeNonLabelChars = R.replace(new RegExp(nonLabelChar, 'i'), '');
-const splitCharacters = R.split('');
-const joinWithOptionalNonLabelChar = R.join(`${nonLabelChar}?`);
-
-/**
- * Creates a regex fragment which will match strings that have the label in
- * them, allowing for punctuation or indiviual spaces to be added or removed.
- */
-const matchAnyPunctuation = R.compose(
-  joinWithOptionalNonLabelChar,
-  splitCharacters,
-  removeNonLabelChars,
-);
-
-/**
- * Creates a regex which is used to determine if a lebel is present.
- * If there are characters immediately before or after the label, they must not
- * be characters which could be part of a label.
- */
-const makeLabelRegex = label => new RegExp(
-  `^(?:.*${nonLabelChar})?${matchAnyPunctuation(label)}(?:${nonLabelChar}.*)?$`,
-  'i',
-);
-
-const manufacturerRegexTest = ({ manufacturer }) => R.compose(
-  R.test(new RegExp(`^${manufacturer}$`, 'i')),
-  R.prop('manufacturer'),
-);
-
-const modelRegexTest = ({ model }) => R.compose(
-  R.test(makeLabelRegex(model)),
-  R.prop('title'),
-);
-
-const familyRegexTest = ({ family }) => R.compose(
-  R.test(family ? makeLabelRegex(family) : /(?:)/),
-  R.prop('title'),
-);
-
-const naiveMatching = product => R.allPass([
-  manufacturerRegexTest(product),
-  modelRegexTest(product),
-  familyRegexTest(product),
-]);
-
-// const mergeInMatchedListings = product => R.compose(
-//   R.merge,
-//   R.objOf('listings'),
-//   R.filter(naiveMatching(product)),
-// );
-
-const makeFilteredListingsObject = product => R.compose(
-  R.objOf('listings'),
-  R.filter(naiveMatching(product)),
-);
-
-
-const augmentProductWithListings = listings => product => R.merge(
-  makeFilteredListingsObject(product)(listings),
-  product,
-);
-
-/**
- * Returns an array of products which have been augmented with an array of
- * listings according to the following heuristic:
- *  - the listing's manufacturer exactly matches the product's manufacturer,
- *    ignoring case.
- *  - the listing's title contains the product's model, allowing for punctuation
- *    and case differences.
- *  - if the product has a famliy, the listings's title contains it, allowing
- *    for punctuation and case differences.
- *
- * @param {[Product]} products
- * @param {[Listing]} listings
- * @returns {[Product]}
- */
 
 const resultsProperties = ['product_name', 'listings'];
 
-const generateResults = ({ products, listings }) =>
-  products
-    .map(augmentProductWithListings(listings))
-    .map(R.pick(resultsProperties));
+// encapsulate business logic inside IIFE
+const generateResults = (() => {
+  /**
+   * A regex fragment that matches any character which does not contirbute
+   * to the meaning of a product label (model/family).
+   */
+  const nonLabelChar = '[^a-z0-9]';
+
+  /**
+   * String -> String
+   *
+   * Creates a regex fragment which will match strings that have the label in
+   * them, allowing for punctuation or indiviual spaces to be added or removed.
+   */
+  const matchAnyPunctuation = R.compose(
+    R.join(`${nonLabelChar}?`),
+    R.split(''),
+    R.replace(new RegExp(nonLabelChar, 'i'), ''),
+  );
+
+  /**
+   * String -> RegExp
+   *
+   * Creates a regex which is used to determine if a lebel is present.
+   * If there are characters immediately before or after the label, they must
+   * not be characters which could be part of a label.
+   */
+  const makeLabelRegex = label => new RegExp(
+    `^(?:.*${nonLabelChar})?${matchAnyPunctuation(label)}(?:${nonLabelChar}.*)?$`,
+    'i',
+  );
+
+  /**
+   * Product -> Listing -> Boolean
+   *
+   * Tests that the listing's manufacturer matches the product's manufacturer.
+   */
+  const manufacturerRegexTest = ({ manufacturer }) => R.compose(
+    R.test(new RegExp(`^${manufacturer}$`, 'i')),
+    R.prop('manufacturer'),
+  );
+
+  /**
+   * Product -> Listing -> Boolean
+   *
+   * Tests that the listing's title contains the product's model.
+   */
+  const modelRegexTest = ({ model }) => R.compose(
+    R.test(makeLabelRegex(model)),
+    R.prop('title'),
+  );
+
+  /**
+   * Product -> Listing -> Boolean
+   *
+   * Tests that if the product has a famliy, the listings's title contains it.
+   */
+  const familyRegexTest = ({ family }) => R.compose(
+    R.test(family ? makeLabelRegex(family) : /(?:)/),
+    R.prop('title'),
+  );
+
+  /**
+   * Product -> Listing -> Boolean
+   *
+   * For a given (Product, Listing) pair - return true iff all three of the
+   * given tests pass.
+   */
+  const naiveMatching = product => R.allPass([
+    manufacturerRegexTest(product),
+    modelRegexTest(product),
+    familyRegexTest(product),
+  ]);
+
+  /**
+   * [IndexedListing] -> Product -> Product'
+   *
+   * returns a product that has been augmented with all listings that naively
+   * match it.
+   */
+  const augmentProductWithListingIndices = R.curry((listings, product) =>
+    Object.assign({}, {
+      listings: listings.reduce(
+        (filtered, listing, index) => (naiveMatching(product)(listing)
+          ? [...filtered, index]
+          : filtered
+        ),
+        [],
+      ),
+      // listings: listings.filter(naiveMatching(product))
+    }, product),
+  );
+
+  const resolveListings = R.curry((listings, product) =>
+    Object.assign({},
+      product,
+      { listings: product.listings.map(index => listings[index]) },
+    ),
+  );
+
+
+  /**
+   * Consumes the products and listings, and outputs results in the specified
+   * shape, after applying a number of tests and heuristics.
+   *
+   * @param {[Product]} products
+   * @param {[Listing]} listings
+   * @returns {[Result]}
+   */
+  return ({ products, listings }) =>
+    products
+      .map(augmentProductWithListingIndices(listings))
+      .map(R.pick(resultsProperties))
+      .map(resolveListings(listings));
+})();
 
 
 /* ----------- ENTRY POINT / FILE PROCESSING ----------- */
